14db48bf8487309113f615a5db8ea6d02a9ffb7f
coss <cossx@yahoo.com>
2023-03-30 18:54:07 +0600

    add table

--- src/test/java/pages/demoqa/TablePage.java
+++ src/test/java/pages/demoqa/TablePage.java
@@ -3,12 +3,12 @@ package pages.demoqa;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.*;
 
 public class TablePage {
-
     WebDriver driver;
 
     public TablePage(WebDriver driver) {
@@ -16,22 +16,70 @@ public class TablePage {
         PageFactory.initElements(driver, this);
     }
 
+    @FindBy(xpath = "//input[@id=\"searchBox\"]")
+    private WebElement searchInput;
 
+    @FindBy(xpath = "//button[@id=\"addNewRecordButton\"]")
+    private WebElement addButton;
 
+    @FindBy(xpath = "//input[@id=\"firstName\"]")
+    private WebElement firstNameInput;
 
+    @FindBy(xpath = "//input[@id=\"lastName\"]")
+    private WebElement lastNameInput;
 
-    public void printData() {
-        List<WebElement> data = driver.findElements(By.xpath("//div[@role=\"rowgroup\"]"));
+    @FindBy(xpath = "//input[@id=\"userEmail\"]")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//input[@id=\"age\"]")
+    private WebElement ageInput;
+
+    @FindBy(xpath = "//input[@id=\"salary\"]")
+    private WebElement salaryInput;
+
+    @FindBy(xpath = "//input[@id=\"department\"]")
+    private WebElement departmentInput;
+
+    @FindBy(xpath = "//button[@id=\"submit\"]")
+    private WebElement submitButton;
+
+    public void clickToAddButton() {
+        addButton.click();
+    }
+
+    public void fillData(String fistName, String lastName, String email, String age, String salary, String department) {
+        firstNameInput.clear();
+        firstNameInput.sendKeys(fistName);
+        lastNameInput.clear();
+        lastNameInput.sendKeys(lastName);
+        emailInput.clear();
+        emailInput.sendKeys(email);
+        ageInput.clear();
+        ageInput.sendKeys(age);
+        salaryInput.clear();
+        salaryInput.sendKeys(salary);
+        departmentInput.clear();
+        departmentInput.sendKeys(department);
+        submitButton.click();
+    }
+
+    public void fillSearchInput(String text) {
+        searchInput.clear();
+        searchInput.sendKeys(text);
+    }
+
+    public List<Map<String, String>> getTableData() {
+        List<WebElement> data = driver.findElements(By.xpath("//div[@role='rowgroup']"));
         List<Map<String, String>> rows = new ArrayList<>();
 
         for(int k=0; k<= data.size()-1; k++) {
             List<String> cell = new ArrayList<>();
-            Map<String, String> dataCell = new HashMap();
-            if(!data.get(k).getText().contains("  ")){
-                for(int i=1; i<=6; i++){
-                    String locator = String.format("//div[@role=\"rowgroup\"][%s]//div[@class='rt-td'][%s]", k + 1, i);
-                    String cellData = driver.findElement(By.xpath(locator)).getText();
-                    cell.add(cellData);
+            Map<String, String> dataCell = new HashMap<>();
+            if(!data.get(k).getText().contains("  ")) {
+                for(int i=1; i<=6; i++) {
+                    String locator = String.format("//div[@role='rowgroup'][%s]//div[@class='rt-td'][%s]", k + 1, i);
+                    String cellText = driver.findElement(By.xpath(locator)).getText();
+                    cell.add(cellText);
                 }
                 dataCell.put("First Name", cell.get(0));
                 dataCell.put("Last Name", cell.get(1));
@@ -41,7 +89,10 @@ public class TablePage {
                 dataCell.put("Department", cell.get(5));
                 rows.add(dataCell);
             }
+
         }
-        System.out.println(rows);
+        return rows;
     }
+
+
 }
--- src/test/java/tests/TableTest.java
+++ src/test/java/tests/TableTest.java
@@ -1,13 +1,25 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.demoqa.TablePage;
+
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
 
 public class TableTest extends TestBase {
     String url = "https://demoqa.com/webtables";
 
     TablePage tablePage;
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String age = String.valueOf(faker.number().numberBetween(10, 100));
+    String email = faker.internet().emailAddress();
+    String salary = String.valueOf(faker.number().numberBetween(10000, 10000000));
+    String department = faker.lorem().characters(7);
+
 
     @BeforeMethod
     public void openUrl() {
@@ -17,7 +29,24 @@ public class TableTest extends TestBase {
     @Test
     public void tableTest() {
         tablePage = new TablePage(driver);
-        tablePage.printData();
+        List<Map<String, String>> data = tablePage.getTableData();
+        System.out.println(data);
+        System.out.println(data.get(1).get("Age"));
+    }
+
+    @Test
+    public void addAndSearchTableData() {
+        tablePage = new TablePage(driver);
+        tablePage.clickToAddButton();
+        tablePage.fillData(firstName, lastName, email, age, salary, department);
+        tablePage.fillSearchInput(firstName);
+        List<Map<String, String>> data = tablePage.getTableData();
+        Assert.assertEquals(data.get(0).get("First Name"), firstName);
+        Assert.assertEquals(data.get(0).get("Last Name"), lastName);
+        Assert.assertEquals(data.get(0).get("Age"), age);
+        Assert.assertEquals(data.get(0).get("Email"), email);
+        Assert.assertEquals(data.get(0).get("Salary"), salary);
+        Assert.assertEquals(data.get(0).get("Department"), department);
     }
 
 }
