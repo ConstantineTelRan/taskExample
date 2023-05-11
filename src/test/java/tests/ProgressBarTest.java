1d4010c9f7b3f3792a90172bdce7c4620bcb86c6
coss <cossx@yahoo.com>
2023-05-11 22:45:52 +0600

    add list of elements

--- src/test/java/tests/ProgressBarTest.java
+++ src/test/java/tests/ProgressBarTest.java
@@ -1,11 +1,11 @@
-package tests;
-
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import pages.demoqa.ProgressBar;
-
-public class ProgressBarTest extends TestBase {
+//package tests;
+//
+//import org.testng.Assert;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//import pages.demoqa.ProgressBar;
+//
+//public class ProgressBarTest extends TestBase {
 //    String url = "https://demoqa.com/progress-bar";
 //    String percent = Integer.toString(faker.number().numberBetween(1, 99));
 //    String progressValue;
@@ -26,5 +26,5 @@ public class ProgressBarTest extends TestBase {
 //        Assert.assertEquals(progressValue, percent);
 //        System.out.println(percent);
 //    }
-
-}
+//
+//}
