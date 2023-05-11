945d2d9bd9b5b05f7af9f36e9329a2802618c298
coss <cossx@yahoo.com>
2023-05-11 22:43:43 +0600

    add list of elements

--- src/test/java/tests/ProgressBarTest.java
+++ src/test/java/tests/ProgressBarTest.java
@@ -6,25 +6,25 @@ import org.testng.annotations.Test;
 import pages.demoqa.ProgressBar;
 
 public class ProgressBarTest extends TestBase {
-    String url = "https://demoqa.com/progress-bar";
-    String percent = Integer.toString(faker.number().numberBetween(1, 99));
-    String progressValue;
-    ProgressBar progressBar;
-
-
-    @BeforeMethod
-    public void precondition() {
-        driver.get(url);
-    }
-
-    @Test
-    public void setProgressBarTest() {
-        progressBar = new ProgressBar(driver);
-        progressBar.clickToStartStopButton();
-        progressBar.clickToStop(percent);
-        progressValue = progressBar.getAttributeValue();
-        Assert.assertEquals(progressValue, percent);
-        System.out.println(percent);
-    }
+//    String url = "https://demoqa.com/progress-bar";
+//    String percent = Integer.toString(faker.number().numberBetween(1, 99));
+//    String progressValue;
+//    ProgressBar progressBar;
+//
+//
+//    @BeforeMethod
+//    public void precondition() {
+//        driver.get(url);
+//    }
+//
+//    @Test
+//    public void setProgressBarTest() {
+//        progressBar = new ProgressBar(driver);
+//        progressBar.clickToStartStopButton();
+//        progressBar.clickToStop(percent);
+//        progressValue = progressBar.getAttributeValue();
+//        Assert.assertEquals(progressValue, percent);
+//        System.out.println(percent);
+//    }
 
 }
