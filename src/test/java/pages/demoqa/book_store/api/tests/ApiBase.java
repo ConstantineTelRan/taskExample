d2fd6b7b2397c6e9203046123bd93a9108732aa1
coss <cossx@yahoo.com>
2023-05-25 13:22:22 +0600

    add apikey

--- src/test/java/pages/demoqa/book_store/api/tests/ApiBase.java
+++ src/test/java/pages/demoqa/book_store/api/tests/ApiBase.java
@@ -6,6 +6,10 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeMethod;
+import pages.demoqa.book_store.api.factories.LoginViewModelFactory;
+import pages.demoqa.book_store.api.model.LoginViewModelDto;
+import pages.demoqa.book_store.api.model.RespGenerateToken;
 
 public class ApiBase {
     protected Faker faker = new Faker();
@@ -16,17 +20,36 @@ public class ApiBase {
     final String BASE_URI = "https://demoqa.com/";
     final String API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6ImFkbWluMjIiLCJwYXNzd29yZCI6IlF3ZXJ0eUAxMjM0NSIsImlhdCI6MTY4NDE0NTA3MH0.KVrfbbc_4ESM84pTNYTa5Fl7adKPWe1mRU55GX_-zJI";
 
+    String apiKey;
+
+
+    @BeforeMethod
+    public void getToken() {
+        LoginViewModelFactory loginViewModelFactory = new LoginViewModelFactory();
+        LoginViewModelDto loginViewModelDto = loginViewModelFactory.getObjectWithAdmin();
+        apiKey = RestAssured.given()
+                .baseUri(BASE_URI)
+                .contentType(ContentType.JSON)
+                .body(loginViewModelDto)
+                .when()
+                .log().all()
+                .post(GENERATE_TOKEN_END_POINT)
+                .then().log().all()
+                .extract().response().as(RespGenerateToken.class).getToken();
+    }
+
 
 
     RequestSpecification spec = new RequestSpecBuilder()
             .setBaseUri(BASE_URI)
             .setContentType(ContentType.JSON)
-            .addHeader("Access-Token", API_KEY)
+//            .addHeader("Access-Token", apiKey)
             .build();
 
     public Response doPostRequest(String endPoint, Integer responseCode, Object body) {
         Response resp = RestAssured.given()
                 .spec(spec)
+                .header("Access-Token", apiKey)
                 .body(body)
                 .when()
                 .log().all()
