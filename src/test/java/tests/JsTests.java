c7c0cf60ee743b45f0c23968dc5f5fb2433d1de3
coss <cossx@yahoo.com>
2023-06-06 16:47:39 +0600

    add date and js

--- src/test/java/tests/JsTests.java
+++ src/test/java/tests/JsTests.java
@@ -113,16 +113,100 @@ public class JsTests extends TestBase {
         System.out.println("kjdksksk");
     }
 
-//    @Test
-//    public void checkbox() {
-//        driver.get("https://demoqa.com/checkbox");
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("document.getElementById('tree-node-home').checked=true");
-//        js.executeScript("document.getElementById('tree-node-home').checked=false");
-//        System.out.println();
-//
-//    }
+    @Test
+    public void checkbox() {
+        driver.get("https://demoqa.com/automation-practice-form");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.getElementById('hobbies-checkbox-1').checked=true");
+        js.executeScript("document.getElementById('hobbies-checkbox-1').checked=false");
+        System.out.println();
+
+    }
+
+    @Test
+    public void checkbox2() {
+        driver.get("https://demoqa.com/checkbox");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement element = driver.findElement(By.xpath("//input[@id=\"tree-node-home\"]"));
+        //тут еще пример как с кнопкой
+        js.executeScript("arguments[0].click();", element);
+        js.executeScript("arguments[0].click();", element);
+        System.out.println();
+
+    }
+
+
 //    TODO разобраться с чекбоксами
+
+    @Test
+    public void dragDrop() throws InterruptedException {
+        driver.get("https://demoqa.com/droppable");
+
+        WebElement sourceElement = driver.findElement(By.id("draggable"));
+        WebElement targetElement = driver.findElement(By.id("droppable"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        String dragAndDropScript = "function createEvent(typeOfEvent) {\n" +
+                "    var event = document.createEvent(\"CustomEvent\");\n" +
+                "    event.initCustomEvent(typeOfEvent, true, true, null);\n" +
+                "    event.dataTransfer = {\n" +
+                "        data: {},\n" +
+                "        setData: function (key, value) {\n" +
+                "            this.data[key] = value;\n" +
+                "        },\n" +
+                "        getData: function (key) {\n" +
+                "            return this.data[key];\n" +
+                "        }\n" +
+                "    };\n" +
+                "    return event;\n" +
+                "}\n" +
+                "\n" +
+                "function dispatchEvent(element, event, transferData) {\n" +
+                "    if (transferData !== undefined) {\n" +
+                "        event.dataTransfer = transferData;\n" +
+                "    }\n" +
+                "    if (element.dispatchEvent) {\n" +
+                "        element.dispatchEvent(event);\n" +
+                "    } else if (element.fireEvent) {\n" +
+                "        element.fireEvent(\"on\" + event.type, event);\n" +
+                "    }\n" +
+                "}\n" +
+                "\n" +
+                "function simulateHTML5DragAndDrop(sourceElement, targetElement) {\n" +
+                "    var dragStartEvent = createEvent('dragstart');\n" +
+                "    dispatchEvent(sourceElement, dragStartEvent);\n" +
+                "    var dropEvent = createEvent('drop');\n" +
+                "    dispatchEvent(targetElement, dropEvent, dragStartEvent.dataTransfer);\n" +
+                "    var dragEndEvent = createEvent('dragend');\n" +
+                "    dispatchEvent(sourceElement, dragEndEvent, dropEvent.dataTransfer);\n" +
+                "}\n" +
+                "\n" +
+                "var source = arguments[0];\n" +
+                "var target = arguments[1];\n" +
+                "simulateHTML5DragAndDrop(source, target);";
+
+        js.executeScript(dragAndDropScript, sourceElement, targetElement);
+
+        Thread.sleep(1500);
+        System.out.println("jdjdjdj");
+    }
+
+    @Test
+    public void DropDown() {
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement dropdownElement = driver.findElement(By.id("dropdown"));
+
+        String selectOptionScript = "arguments[0].selectedIndex = INDEX; arguments[0].dispatchEvent(new Event('change'));";
+        int optionIndex = 2; // Индекс выбранного элемента (нумерация начинается с 0)
+
+        js.executeScript(selectOptionScript.replace("INDEX", String.valueOf(optionIndex)), dropdownElement);
+
+        System.out.println("jdjdjdj");
+
+    }
+
 
 
 }
